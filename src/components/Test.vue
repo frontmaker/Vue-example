9b759ab75130f33d7f9a873fceb7ef447e093fe1
frontmaker <Djeka345@gmail.com>
2017-02-12 22:34:55 +0300

    Initial commit

---
+++ src/components/Test.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="test">
+    <p v-bind:title="msg">Message</p>
+    <ul v-for="item in items">
+        <li class="item">{{ item }}</li>
+    </ul>
+    <p v-if="seen">Show</p>
+    <button v-on:click="changeVisibility">Click me</button>
+  </div>
+</template>
+
+
+<script>
+
+  export default {
+    name: 'test',
+    data () {
+      return {
+        msg: 'Message',
+        seen: true,
+        items: [1, 2, 3, 4, 5]
+      }
+    },
+    methods: {
+      changeVisibility () {
+        this.seen = !this.seen
+      }
+    }
+  }
+
+</script>
+
+
+<style>
+
+  .item {
+    list-style-type: none;
+  }
+
+
+
+</style>
